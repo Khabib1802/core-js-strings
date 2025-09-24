89299c06ab529bc2031c2ec945429a4a2707c194
Khabib Gadzhimagomedov <habib31.03.2002@gmail.com>
2025-09-24 18:27:38 +0300

    feat: solve tasks 9-10

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,11 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const valueIndex = str.indexOf(value);
+  return str.includes(value)
+    ? `${str.slice(0, valueIndex)}${str.slice(valueIndex + value.length)}`
+    : str;
 }
 
 /**
@@ -162,8 +165,11 @@ function removeFirstOccurrences(/* str, value */) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const valueLastIndex = str.lastIndexOf(value);
+  return str.includes(value)
+    ? `${str.slice(0, valueLastIndex)}${str.slice(valueLastIndex + value.length)}`
+    : str;
 }
 
 /**
